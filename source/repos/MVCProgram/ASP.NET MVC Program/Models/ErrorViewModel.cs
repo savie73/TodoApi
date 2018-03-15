ee8051d976fdc442a18790667da03eda1f2455f7
Savannah Pyle <bea12022@byui.edu>
2018-03-15 11:40:09 -0600

    added controller

---
+++ source/repos/MVCProgram/ASP.NET MVC Program/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP.NET_MVC_Program.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
