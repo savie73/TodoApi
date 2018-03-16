526a70468ff252999a95aed52200bbd87da91f3d
Savannah Pyle <bea12022@byui.edu>
2018-03-16 17:38:11 -0600

    added search, new field and validation

--- source/repos/MVCProgram/ASP.NET MVC Program/Models/Movie.cs
+++ source/repos/MVCProgram/ASP.NET MVC Program/Models/Movie.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MvcMovie.Models
 {
@@ -10,9 +7,12 @@ namespace MvcMovie.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
+
     }
 }
