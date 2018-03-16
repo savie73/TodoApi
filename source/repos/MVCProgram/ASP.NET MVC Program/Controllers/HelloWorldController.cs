2796ec708eddc9128e9cd4144118ed816b734ba0
Savannah Pyle <bea12022@byui.edu>
2018-03-15 21:04:19 -0600

    added view and controller extra, just added moviescontroller

--- source/repos/MVCProgram/ASP.NET MVC Program/Controllers/HelloWorldController.cs
+++ source/repos/MVCProgram/ASP.NET MVC Program/Controllers/HelloWorldController.cs
@@ -11,20 +11,31 @@ namespace MvcMovie.Controllers
         // 
         // GET: /HelloWorld/
 
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
 
         // 
         // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome(string name, int ID = 1)
-        {
-            //return "This is the Welcome action method...";
-            //return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        /*   public string Welcome(string name, int ID = 1)
+           {
+               return "This is the Welcome action method...";
+               return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+               return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
 
+           } */
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
+
+
+
     }
 }
--- source/repos/MVCProgram/ASP.NET MVC Program/Startup.cs
+++ source/repos/MVCProgram/ASP.NET MVC Program/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
 
 namespace ASP.NET_MVC_Program
 {
@@ -22,6 +24,9 @@ namespace ASP.NET_MVC_Program
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<MvcMovieContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
